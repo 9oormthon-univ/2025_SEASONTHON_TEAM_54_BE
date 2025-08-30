266f8fa7b5735cf9cf5354aca24b0f142dda2a10
Santoragi <woojj1254577@naver.com>
2025-08-30 15:24:23 +0900

    setting: 스프링부트 프로젝트 생성

---
+++ src/main/java/com/example/ssg_tab/SsgTabApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssg_tab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgTabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgTabApplication.class, args);
+	}
+
+}
