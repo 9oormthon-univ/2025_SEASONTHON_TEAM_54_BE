e6ec8542a08649a9c67b4965f0c3f02a865219cb
Santoragi <woojj1254577@naver.com>
2025-09-05 23:28:22 +0900

    setting/#30 : 시큐리티 설정 수정

--- src/main/java/com/example/ssg_tab/global/config/SecurityConfig.java
+++ src/main/java/com/example/ssg_tab/global/config/SecurityConfig.java
@@ -31,11 +31,9 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",      // 혹시 모를 기본 경로도 함께 허용
                                 "/auth/**",             // 인증 경로
                                 "/dev/**",               // 개발용 API
+                                "/test/**",              // 테스트용 API
                                 "/**"
                         ).permitAll()
-
-                        // 테스트용
-                        .requestMatchers("/test/**").permitAll()
 
                         // 그 외는 인증 필요
                         .anyRequest().authenticated()
